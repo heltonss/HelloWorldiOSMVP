2ee3de55675a59f996d10fce1b57db7cd0c79b55
Helton Silveira <helton@heltonsouza.com.br>
2024-12-30 14:25:16 -0300

    Initial Commit

---
+++ HelloWorldGrettingsTests/HelloWorldGrettingsTests.swift
@@ -0,0 +1,17 @@
+//
+//  HelloWorldGrettingsTests.swift
+//  HelloWorldGrettingsTests
+//
+//  Created by helton souza silveira on 30/12/24.
+//
+
+import Testing
+@testable import HelloWorldGrettings
+
+struct HelloWorldGrettingsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
